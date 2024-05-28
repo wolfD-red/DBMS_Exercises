2e7bb158c431db61b54881c56e1a97bda5dc6c6d
AkuuNoro <marabilla.rr.bscs@gmail.com>
2024-05-28 12:28:17 +0800

    Done 05/28/2024

--- (2)Program Exercise 1 & 2/Program_Exercise_1and2.cpp
+++ (2)Program Exercise 1 & 2/Program_Exercise_1and2.cpp
@@ -7,26 +7,34 @@ using namespace std;
 times within 10 times. and can be repeated but 
 this time 10 times within 3 times.*/
 
+void after_choice_clear();
 
 int main (){
 
     string uname;
+    do {
+        cout << endl;
+        cout << "Input Your Name: ";
+        getline (cin, uname);
+        after_choice_clear();
 
-    cout << "Input Your Name: ";
-    getline (cin, uname);
-
-    for (int i = 1; i <= 3; i++){
+        for (int i = 1; i <= 3; i++){
             cout << endl;
-            cout << "[" << i << "]" << endl;
+            cout << "--[" << i << "]--" << endl;
             cout << endl;
-        for (int j = 1; j <= 10; j++){
+            for (int j = 1; j <= 10; j++){
                 cout << endl;
                 cout << "[" << j << "]" << endl;
-            for (int k = 1; k <= 5; k++){
+                for (int k = 1; k <= 5; k++){
                 cout << uname << endl;
+                }
             }
         }
-    }
+    } while (1);
 
     return 0;
+}
+
+void after_choice_clear (){
+    system ("cls");
 }
